6884fbdbe9a0ce4240aaf865fdc294565e344ae4
dharm1004 <dharms1004@gmail.com>
2021-05-12 17:47:39 +0530

    withdraw-module

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -64,6 +64,32 @@
 
             <li class="nav-item">
                 <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-copy"></i>
+                    <p>
+                        Trnsactions
+                        <i class="fas fa-angle-left right"></i>
+                        <span class="badge badge-info right">6</span>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="{{ route('withdraw-list') }}" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Withdraw</p>
+                        </a>
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a href="{{ route('createOffer') }}" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Create Offers</p>
+                        </a>
+                    </li> -->
+                </ul>
+            </li>
+
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
                     <i class="nav-icon fas fa-edit"></i>
                     <p>
                         Admin Settings
